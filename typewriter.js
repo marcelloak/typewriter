11e5f115762d0a8eaead5c1add274c8679b090f4
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-21 21:10:03 +0000

    Updated

--- typewriter.js
+++ typewriter.js
@@ -3,7 +3,7 @@ const typewriter = function(sentence) {
   for (const char of sentence) {
     setTimeout(() => {
       process.stdout.write(char);
-    }, 1000 + counter) // <= 1s delay to make it noticeable. Can dial it down later.
+    }, 1000 + counter)
     counter += 50;
   }
   setTimeout(() => {
